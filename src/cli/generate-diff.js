d0aaa1f9e7c72554f5516a06b875be9906707682
serhii.shramko <shramko.web@yahoo.com>
2020-06-15 17:09:58 +0300

    feat: WIP add comparing files arguments to cli

--- src/cli/generate-diff.js
+++ src/cli/generate-diff.js
@@ -5,7 +5,12 @@ import packageJsonFile from '../../package.json';
 const generateDiff = (processArgv) => {
   commander
     .version(packageJsonFile.version)
-    .description(packageJsonFile.description);
+    .description(packageJsonFile.description)
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
+      console.log('test: ', filepath1, filepath2);
+    });
 
   commander.parse(processArgv);
 };
