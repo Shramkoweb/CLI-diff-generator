4e9f4090d0398ee9bc5b43ff07d9047501216499
serhii.shramko <shramko.web@yahoo.com>
2020-06-17 20:33:56 +0300

    refactor: rewrite bin

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node --experimental-json-modules
+import commander from 'commander';
 
 import generateDiff from '../src/index.js';
+import packageJsonFile from '../package.json';
 
-generateDiff(process.argv);
+commander
+  .version(packageJsonFile.version)
+  .description(packageJsonFile.description)
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((firstFilePath, secondFilePath) => {
+    console.log(generateDiff(firstFilePath, secondFilePath));
+  });
+
+commander.parse(process.argv);
