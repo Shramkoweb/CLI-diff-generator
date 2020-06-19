3a494414a8e0abc09d8df41c595cd8b6e1d8c803
serhii.shramko <shramko.web@yahoo.com>
2020-06-20 00:17:31 +0300

    feat: implement ini parsing add tests & ini fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,14 +5,21 @@ import generateDiff from '../src/cli/generate-diff';
 
 const dirname = path.dirname(new URL(import.meta.url).pathname);
 
+// json
 const beforePathToJSON = path.resolve(dirname, '__fixtures__/before.json');
 const afterPathToJSON = path.resolve(dirname, '__fixtures__/after.json');
+// yaml
 const beforePathToYAML = path.resolve(dirname, '__fixtures__/before.yaml');
 const afterPathToYAML = path.resolve(dirname, '__fixtures__/after.yaml');
+// ini
+const beforePathToINI = path.resolve(dirname, '__fixtures__/before.ini');
+const afterPathToINI = path.resolve(dirname, '__fixtures__/after.ini');
+
+// reference result
 const resultPath = path.resolve(dirname, '__fixtures__/compareResult.txt');
 
 describe('Test generateDiff function', () => {
-  test('Should correct compare JSON', () => {
+  test('Should correct compare .json', () => {
     const firstFilePath = path.resolve(beforePathToJSON);
     const secondFilePath = path.resolve(afterPathToJSON);
     const resultFilePath = path.resolve(resultPath);
@@ -20,9 +27,19 @@ describe('Test generateDiff function', () => {
     expect(generateDiff(firstFilePath, secondFilePath))
       .toBe(fs.readFileSync(resultFilePath, 'utf-8'));
   });
-  test('Should correct compare YAML', () => {
+
+  test('Should correct compare .yaml', () => {
     const firstFilePath = path.resolve(beforePathToYAML);
     const secondFilePath = path.resolve(afterPathToYAML);
+    const resultFilePath = path.resolve(resultPath);
+
+    expect(generateDiff(firstFilePath, secondFilePath))
+      .toBe(fs.readFileSync(resultFilePath, 'utf-8'));
+  });
+
+  test('Should correct compare .ini', () => {
+    const firstFilePath = path.resolve(beforePathToINI);
+    const secondFilePath = path.resolve(afterPathToINI);
     const resultFilePath = path.resolve(resultPath);
 
     expect(generateDiff(firstFilePath, secondFilePath))
