64b7e9ba01413b9af5620b3932d3d8fe8922373e
serhii.shramko <shramko.web@yahoo.com>
2020-06-21 22:07:05 +0300

    refactor: stylish.js

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -50,59 +50,57 @@ const first = {
 
 const singleIndentSpaceCount = 4;
 
-const stringify = (data, treeDepth) => {
-  const op = ' '.repeat((treeDepth + 1) * singleIndentSpaceCount + singleIndentSpaceCount);
-  const ident = ' '.repeat(treeDepth * singleIndentSpaceCount + singleIndentSpaceCount);
-
-  const stringifyObject = (object) => {
-    const string = _.keys(object)
-      .map((key) => `${op}${key}:  ${stringify(object[key])}`)
-      .join('\n');
-
-    return `{\n${string}\n${ident}}`;
-  };
-  return _.isObject(data) ? stringifyObject(data) : `${data}`;
-};
-
-const nodeStateToFormatting = {
-  [nodeState.added]: ({ name, value }, treeDepth) => {
-    const ident = ' '.repeat(treeDepth * singleIndentSpaceCount + singleIndentSpaceCount / 2);
-
-    return `${ident}+ ${name}: ${stringify(value, treeDepth)}`;
-  },
-  [nodeState.deleted]: ({ name, value }, treeDepth) => {
-    const ident = ' '.repeat(treeDepth * singleIndentSpaceCount + singleIndentSpaceCount / 2);
-
-    return `${ident}- ${name}: ${stringify(value, treeDepth)}`;
-  },
-  [nodeState.unchanged]: ({ name, value }, treeDepth) => {
-    const ident = ' '.repeat(treeDepth * singleIndentSpaceCount + singleIndentSpaceCount);
-
-    return `${ident}${name}: ${stringify(value, treeDepth)}`;
-  },
-  [nodeState.nested]: ({ name, children }, treeDepth) => {
-    const ident = ' '.repeat(treeDepth * singleIndentSpaceCount + singleIndentSpaceCount);
-
-    return `${ident}${name}: ${makeStylishFormat(children, treeDepth + 1)}`;
-  },
-  [nodeState.changed]: (node, treeDepth) => {
-    const { name, addedValue, removedValue } = node;
-    const addedString = nodeStateToFormatting[nodeState.added]({ name, value: addedValue }, treeDepth);
-    const deletedString = nodeStateToFormatting[nodeState.deleted]({ name, value: removedValue }, treeDepth);
-
-    return [addedString, deletedString];
-  },
-};
+const getIndent = (depth) => ' '.repeat(depth);
 
 const makeStylishFormat = (diff, treeDepth = 0) => {
-  const braceIndent = ' '.repeat(treeDepth * singleIndentSpaceCount);
+  const defaultIndentLength = treeDepth * singleIndentSpaceCount;
+
+  const largeIndent = getIndent(defaultIndentLength + singleIndentSpaceCount);
+  const nestedObjectIndent = getIndent((treeDepth + 1) * singleIndentSpaceCount + singleIndentSpaceCount);
+  const smallIndent = getIndent(defaultIndentLength + singleIndentSpaceCount / 2);
+  const nestedIdent = getIndent(defaultIndentLength);
+
+  const stringify = (data, treeDepth) => {
+    const stringifyObject = (object) => {
+      const string = _.keys(object)
+        .map((key) => `${nestedObjectIndent}${key}:  ${stringify(object[key])}`)
+        .join('\n');
+
+      return `{\n${string}\n${largeIndent}}`;
+    };
+    return _.isObject(data) ? stringifyObject(data) : `${data}`;
+  };
+
+  const nodeStateToFormatting = {
+    [nodeState.added]: ({ name, value }, treeDepth) => {
+      return `${smallIndent}+ ${name}: ${stringify(value, treeDepth)}`;
+    },
+    [nodeState.deleted]: ({ name, value }, treeDepth) => {
+      return `${smallIndent}- ${name}: ${stringify(value, treeDepth)}`;
+    },
+    [nodeState.unchanged]: ({ name, value }, treeDepth) => {
+      return `${largeIndent}${name}: ${stringify(value, treeDepth)}`;
+    },
+    [nodeState.nested]: ({ name, children }, treeDepth) => {
+      return `${largeIndent}${name}: ${makeStylishFormat(children, treeDepth + 1)}`;
+    },
+    [nodeState.changed]: (node, treeDepth) => {
+      const { name, addedValue, removedValue } = node;
+      const addedString = nodeStateToFormatting[nodeState.added]({ name, value: addedValue }, treeDepth);
+      const deletedString = nodeStateToFormatting[nodeState.deleted]({ name, value: removedValue }, treeDepth);
+
+      return [addedString, deletedString];
+    },
+  };
+  /**/
 
   const formattedDiff = diff
     .map((element) => nodeStateToFormatting[element.state](element, treeDepth))
     .flat()
     .join('\n');
+  const bracketsIndent = getIndent(treeDepth);
 
-  return `{\n${formattedDiff}\n${braceIndent}}`;
+  return `{\n${formattedDiff}\n${nestedIdent}}`;
 };
 
 console.log(makeStylishFormat(generateAst(first, second)));
