3c67cd9bee8b1da9cf65d49e063c763ed7dbe173
serhii.shramko <shramko.web@yahoo.com>
2020-06-22 20:02:55 +0300

    test: update gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,14 @@
 import path from 'path';
 import fs from 'fs';
 
-import generateDiff from '../src/cli/generate-diff';
-import makeStylishFormat from '../src/formaters/stylish';
-import generateAst from '../src/ast';
+import generateDiff from '../src/index';
+
+// custom dirname
+const dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // utils
-const dirname = path.dirname(new URL(import.meta.url).pathname);
 const getFixturePath = (fileName) => path.resolve(dirname, '__fixtures__/', `${fileName}`);
-const readFileContent = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf-8');
+const readFileContent = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf-8').trim();
 
 describe('Test generateDiff function', () => {
   test.each([
@@ -18,17 +18,8 @@ describe('Test generateDiff function', () => {
   ])('generateDiff(%s)', (fileFormat) => {
     const pathToFile1 = getFixturePath(`before.${fileFormat}`);
     const pathToFile12 = getFixturePath(`after.${fileFormat}`);
-    const expected = readFileContent(getFixturePath('compareResult.txt'));
+    const expected = readFileContent(getFixturePath('stylishResult.txt'));
 
-    expect(generateDiff(pathToFile1, pathToFile12)).toEqual(expected);
-  });
-
-  test('asdas', () => {
-
-    const pathToFile1 = getFixturePath(`before-nested.json`);
-    const pathToFile12 = getFixturePath(`after-nested.json`);
-    const expected = readFileContent(getFixturePath('compareResult.txt'));
-
-    expect(makeStylishFormat(generateAst(pathToFile1, pathToFile12))).toEqual(expected);
+    expect(generateDiff(pathToFile1, pathToFile12, 'stylish')).toEqual(expected);
   });
 });
