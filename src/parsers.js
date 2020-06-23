3fc8cdac622d21d621f16d9440e0620b70280106
serhii.shramko <shramko.web@yahoo.com>
2020-06-23 11:57:36 +0300

    fix: correct ini parser fix number to string convert in ini parser

--- src/parsers.js
+++ src/parsers.js
@@ -2,12 +2,39 @@ import yaml from 'js-yaml';
 import path from 'path';
 import fs from 'fs';
 import ini from 'ini';
+import _ from 'lodash';
 
-// TODO ini convert number to string need refactor
+const iniParse = (data) => {
+  const parsedData = ini.parse(data);
+
+  const checkNumbers = (obj) => {
+    const keys = _.keys(obj);
+
+    return keys.reduce((acc, key) => {
+      const value = obj[key];
+
+      if (_.isObject(value)) {
+        acc[key] = checkNumbers(value);
+        return acc;
+      }
+
+      if (Number(value) && !_.isBoolean(value)) {
+        acc[key] = Number(value);
+        return acc;
+      }
+
+      acc[key] = obj[key];
+      return acc;
+    }, {});
+  };
+
+  return checkNumbers(parsedData);
+};
+
 const ParserType = {
   '.json': JSON.parse,
   '.yaml': yaml.safeLoad,
-  '.ini': ini.parse,
+  '.ini': iniParse,
 };
 
 // TODO parser should not know about filePath refactor to pure func
