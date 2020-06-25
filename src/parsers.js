5e7655375a457486c18f8e0423e32f2324d78adb
serhii.shramko <shramko.web@yahoo.com>
2020-06-25 22:23:28 +0300

    refactor: rewrite convertStringsToNumbers native reducer to lodash

--- src/parsers.js
+++ src/parsers.js
@@ -1,33 +1,32 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
-import entries from 'lodash/entries';
 import isObject from 'lodash/isObject';
+import reduce from 'lodash/reduce';
 
 import { fileFormats } from './constants.js';
 
 const parseIni = (data) => {
   const parsedData = ini.parse(data);
 
-  const convertStringsToNumbers = (obj) => entries(obj)
-    .reduce((acc, [key, value]) => {
-      if (isObject(value)) {
-        acc[key] = convertStringsToNumbers(value);
-        return acc;
-      }
-
-      /* ini библиотека во время парсинга подменяет числа на строки
-      *  здесь я конвертирую строки в число, и если это например '11'
-      * то мы получим число 11, иначе это другой типа
-      * и мы его не трогаем */
-      const parsed = parseInt(value, 10);
-      if (parsed) {
-        acc[key] = parsed;
-        return acc;
-      }
-
-      acc[key] = obj[key];
+  const convertStringsToNumbers = (obj) => reduce(obj, (acc, value, key) => {
+    if (isObject(value)) {
+      acc[key] = convertStringsToNumbers(value);
       return acc;
-    }, {});
+    }
+
+    /* ini библиотека во время парсинга подменяет числа на строки
+    *  здесь я конвертирую строки в число, и если это например '11'
+    * то мы получим число 11, иначе это другой типа
+    * и мы его не трогаем */
+    const parsed = parseInt(value, 10);
+    if (parsed) {
+      acc[key] = parsed;
+      return acc;
+    }
+
+    acc[key] = obj[key];
+    return acc;
+  }, {});
 
   return convertStringsToNumbers(parsedData);
 };
