e90717aff2034214d7c7aa24d4e5c56a4d18d12f
serhii.shramko <shramko.web@yahoo.com>
2020-06-25 22:13:53 +0300

    refactor: convertStringsToNumbers remove redundant obj colling

--- src/parsers.js
+++ src/parsers.js
@@ -1,6 +1,6 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
-import keys from 'lodash/keys';
+import entries from 'lodash/entries';
 import isObject from 'lodash/isObject';
 
 import { fileFormats } from './constants.js';
@@ -8,27 +8,26 @@ import { fileFormats } from './constants.js';
 const parseIni = (data) => {
   const parsedData = ini.parse(data);
 
-  const convertStringsToNumbers = (obj) => keys(obj).reduce((acc, key) => {
-    const value = obj[key];
+  const convertStringsToNumbers = (obj) => entries(obj)
+    .reduce((acc, [key, value]) => {
+      if (isObject(value)) {
+        acc[key] = convertStringsToNumbers(value);
+        return acc;
+      }
 
-    if (isObject(value)) {
-      acc[key] = convertStringsToNumbers(value);
+      /* ini библиотека во время парсинга подменяет числа на строки
+      *  здесь я конвертирую строки в число, и если это например '11'
+      * то мы получим число 11, иначе это другой типа
+      * и мы его не трогаем */
+      const parsed = parseInt(value, 10);
+      if (parsed) {
+        acc[key] = parsed;
+        return acc;
+      }
+
+      acc[key] = obj[key];
       return acc;
-    }
-
-    /* ini библиотека во время парсинга подменяет числа на строки
-    *  здесь я конвертирую строки в число, и если это например '11'
-    * то мы получим число 11, иначе это другой типа
-    * и мы его не трогаем */
-    const parsed = parseInt(value, 10);
-    if (parsed) {
-      acc[key] = parsed;
-      return acc;
-    }
-
-    acc[key] = obj[key];
-    return acc;
-  }, {});
+    }, {});
 
   return convertStringsToNumbers(parsedData);
 };
