e0c3f032bf2e38191ed5b61a7511b82cc515925b
serhii.shramko <shramko.web@yahoo.com>
2020-07-02 21:53:47 +0300

    refactor: fix relative path in utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,13 @@
-import path from 'path';
+import path, { dirname } from 'path';
 import fs from 'fs';
 
+import { fileURLToPath } from 'url';
 import { EXTENSION_MAPPING } from './constants.js';
 
-export const getFixturePath = (fileName) => path.resolve('__tests__', '__fixtures__', fileName);
+const filename = fileURLToPath(import.meta.url);
+const dir = dirname(filename);
+
+export const getFixturePath = (fileName) => path.resolve(dir, '../', '__tests__', '__fixtures__', fileName);
 
 export const getFixtureContent = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf-8').trim();
 
